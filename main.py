4f2afce5d72e0307a4eb838f1911825561d30b0a
Seth Collins <blismatic@gmail.com>
2024-07-23 12:03:06 -0700

    verify getting records from catalog works

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+from knackly_api import KnacklyAPI
+
+
 def main():
-    pass
+    load_dotenv()
+
+    knackly = KnacklyAPI(
+        key_id=os.getenv("KEY"),
+        secret=os.getenv("SECRET"),
+        tenancy=os.getenv("TENANCY"),
+    )
+
+    last_modified = {"c": "after", "v": "2024-07-17T00:00"}
+    x = knackly.get_records_in_catalog(
+        catalog="Transactional", last_modified=last_modified, limit=3, status="Ok"
+    )
+    print(type(x))
+    print(len(x))
 
 
 if __name__ == "__main__":
